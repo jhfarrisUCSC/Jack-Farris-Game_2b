ace9f8ac48174cdcb2625edef7b763cf3559f81c
jhfarrisUCSC <jhfarris@ucsc.edu>
2026-05-12 01:31:30 +0000

    commit -m "outline"

---
+++ src/main.js
@@ -0,0 +1,32 @@
+// Jack Farris
+// Created: 5/1/2025
+// Phaser: 3.70.0
+//
+// Gallery Shooter
+//
+// I copy and pasted this pain
+// 
+// Art assets from Kenny Assets "Pixel Shmup" set:
+// https://kenney.nl/assets/pixel-shmup
+
+// debug with extreme prejudice
+"use strict"
+
+// game config
+let config = {
+    parent: 'phaser-game',
+    type: Phaser.CANVAS,
+    render: {
+        pixelArt: true  // prevent pixel art from getting blurred when scaled
+    },
+    width: 112,
+    height: 192,
+    scene: [gameplay]
+}
+
+// A convenience object that contains all sprites
+// To use: my.sprite.sprite_name
+// Where sprite_name is the name of a specific sprite.
+let my = {sprite: {}};
+
+const game = new Phaser.Game(config);
---
+++ src/scenes/gameplay.js
@@ -0,0 +1,162 @@
+const playerTypes = {
+  tank: {
+    texture: '',
+    health: 50,
+    speed: 5,
+    damage: 1
+  },
+  fort: {
+    texture: '',
+    health: 50,
+    type: ''
+  }
+};
+
+const enemyTypes = {
+  green: {
+    type: "basic",
+    texture: '',
+    health: 1,
+    level: 1,
+  },
+  red: {
+    type: "move",
+    texture: '',
+    health: 1,
+    level: 1,
+    start: [0,0],
+    end: [0,0]
+  },
+  blue: {
+    type: "dive",
+    texture: '',
+    health: 1,
+    level: 1,
+    start: [0,0],
+    middle: [0,0],
+    end: [0,0]
+  },
+  yellow: {
+    type: "fighter",
+    texture: '',
+    health: 1,
+    level: 1,
+    start: [0,0],
+    end: [0,0]
+  }
+};
+
+
+class Gameplay extends Phaser.Scene {
+  constructor() {
+    super("gameplayScene");
+    this.my = {sprite: {}};
+
+    //player stuff
+    this.bodyX = 300;
+    this.bodyY = 500;
+    this.attacking = false;
+    this.attackX = 500;
+    this.attackY = 300;
+
+    // waves
+    this.counter = 0;
+    this.rowX = 0;
+    this.rowY = 0;
+    this.totalRows = 0;
+    this.enemyLevel = 0;
+    this.enemyRow = 0;
+  }
+
+  preload() {
+    this.load.setPath("./assets/");
+
+    // Load Tiles
+    this.load.atlasXML("tileSprites", "tiles_packed.png", "tiles_packed.xml");
+
+    // Load character
+    this.load.atlasXML("planeSprites", "ships_packed.png", "ships_packed.xml");
+
+    document.getElementById('description').innerHTML= '<h2>gameplayScene.js</h2>'
+  }
+
+  create() {
+    let my = this.my;
+
+    // player inputs
+    this.spaceKey =
+    this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+    this.aKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
+    this.dKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
+    my.sprite.body = this.add.sprite(this.bodyX, this.bodyY, "body")
+    my.sprite.attack = this.add.sprite(this.bodyX, this.bodyY, "attack")
+    my.sprite.attack.visible = false;
+
+    this.spaceKey.on('down', () => {
+      if (this.attacking == false) {
+        this.attackX = this.bodyX;
+        this.attackY = this.bodyY;
+        my.sprite.attack.x = this.attackX;
+        my.sprite.attack.y = this.attackY;
+        this.attacking = true;
+        my.sprite.attack.visible = true;
+      }
+    })
+
+    // waves
+    this.totalRows = Math.ceil(Math.random() * 5);
+    for (let i = 0; i < this.totalRows; i++){
+      this.enemyLevel = Math.ceil(Math.random() * 3);
+      this.enemyRow = Math.ceil(Math.random() * 4);
+      switch(enemyRow) {
+        case 1:
+          for (let j = 0; j < 7; i++){
+            // Add Green
+          }
+          break;
+          case 2:
+          for (let j = 0; j < 4; i++){
+            // Add red
+          }
+          break;
+          case 3:
+          for (let j = 0; j < 7; i++){
+            // Add blue
+          }
+          break;
+          case 4:
+          for (let j = 0; j < 3; i++){
+            // Add yellow
+          }
+          break;
+      }
+    }
+
+    // enemy shenanigans
+  }
+
+  update() {
+
+    // tank controls
+    let my = this.my;
+    if (this.aKey.isDown && (my.sprite.body.x > 0)) {
+    this.bodyX -=1;
+    }
+    if (this.dKey.isDown && (my.sprite.body.x < 96)) {
+      this.bodyX +=1;
+    }
+    my.sprite.body.x = this.bodyX;
+    if (this.attacking == true) {
+      this.attackY -= 8;
+    }
+    my.sprite.attack.y = this.attackY;
+    if (my.sprite.attack.y <= 0) {
+      my.sprite.attack.visible = false;
+      this.attacking = false;
+    }
+
+    // counter
+    this.counter++;
+
+  }
+}
